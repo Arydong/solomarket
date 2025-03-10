286396187963d66a7e4c7d625a6a0f5e47356f7b
Arydong <dlehsk43@gmail.com>
2025-03-10 18:12:34 +0900

    25.03.10.(월) 18:12 git, db 연동 완료

---
+++ src/main/java/com/solomarket/controller/MainController.java
@@ -0,0 +1,23 @@
+package com.solomarket.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * packageName    : com.solomarket.controller
+ * fileName       : MainController
+ * author         : 이동하
+ * date           : 25. 3. 10.
+ * description    :
+ * ===========================================================
+ * DATE              AUTHOR             NOTE
+ * -----------------------------------------------------------
+ * 25. 3. 10.        이동하       최초 생성
+ */
+@Controller
+public class MainController {
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
+}
