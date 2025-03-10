1feefc57cee5eff5c1fcd272a8508c7c4a66319f
Arydong <dlehsk43@gmail.com>
2025-03-10 17:12:48 +0900

    first commit

---
+++ src/main/java/com/solomarket/SolomarketApplication.java
@@ -0,0 +1,13 @@
+package com.solomarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SolomarketApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SolomarketApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/solomarket/SolomarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.solomarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SolomarketApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
