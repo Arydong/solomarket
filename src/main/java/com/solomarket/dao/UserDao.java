e4c655b00c38171703364123e23337cb982d06c2
Arydong <dlehsk43@gmail.com>
2025-04-16 17:56:42 +0900

    25.04.16.(화) 17:56 개인정보 수정, 아이디 찾기 완성

--- src/main/java/com/solomarket/dao/UserDao.java
+++ src/main/java/com/solomarket/dao/UserDao.java
@@ -3,6 +3,7 @@ package com.solomarket.dao;
 import com.solomarket.dto.UserDto;
 import com.solomarket.security.CustomUserDetails;
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  *packageName    : com.solomarket.dao
@@ -21,5 +22,9 @@ public interface UserDao {
     UserDto findById(String userId); // 아이디 중복 검사
     UserDto findByNick(String nickName); // 닉네임 중복 검사
     int insertUser(UserDto user); // 회원가입
-//    int countPurchasesByUser(int userId);
+    UserDto findByNameAndPhone(@RequestParam("userName") String userName, @RequestParam("phone") String phone);
+    UserDto findByIdAndPhone(String userId, String phone);
+    int updatePassword(UserDto userDto); // 비밀번호 변경용
+    int updateUserNickAndPassword(UserDto userDto);
+
 }
